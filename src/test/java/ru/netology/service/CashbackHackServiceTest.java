5a5bb913fba59d34cc7693cfbded3533bf103c6e
Evgeniy Gornakov <gornakovevgenij@yandex.ru>
2024-10-07 13:31:54 +0800

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+
+import org.testng.Assert;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemainWithCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemain1WithCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemain999WithCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+}
